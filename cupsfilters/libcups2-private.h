58af46c191269fbede3769a41d58e71d121da1eb
Till Kamppeter <till.kamppeter@gmail.com>
2023-10-25 19:28:57 +0200

    Added "#ifdef HAVE_LIBCUPS2" to libcups2-private.h

--- cupsfilters/libcups2-private.h
+++ cupsfilters/libcups2-private.h
@@ -10,32 +10,31 @@
 #include<config.h>
 
 #ifndef _LIBCUPS2_PRIVATE_H_
-# define _LIBCUPS2_PRIVATE_H_
-#  include "cups/http.h"
-#  include "cups/array.h"
-#  include "cups/cups.h"
-#  include "cups/ipp.h"
-#  include "cups/raster.h"
+#  define _LIBCUPS2_PRIVATE_H_
 
+#  ifdef HAVE_LIBCUPS2 
+#    include "cups/http.h"
+#    include "cups/array.h"
+#    include "cups/cups.h"
+#    include "cups/ipp.h"
+#    include "cups/raster.h"
 
-#  define cupsArrayGetCount      cupsArrayCount
-#  define cupsArrayGetFirst      cupsArrayFirst
-#  define cupsArrayGetNext       cupsArrayNext
-#  define cupsArrayGetElement    cupsArrayIndex
-#  define cupsArrayNew           cupsArrayNew3
-#  define cupsGetDests           cupsGetDests2
-#  define cupsRasterReadHeader   cupsRasterReadHeader2
-#  define cupsRasterWriteHeader  cupsRasterWriteHeader2
-#  define httpConnect            httpConnect2
-#  define ippGetFirstAttribute   ippFirstAttribute
-#  define ippGetNextAttribute    ippNextAttribute
+#    define cupsArrayGetCount      cupsArrayCount
+#    define cupsArrayGetFirst      cupsArrayFirst
+#    define cupsArrayGetNext       cupsArrayNext
+#    define cupsArrayGetElement    cupsArrayIndex
+#    define cupsArrayNew           cupsArrayNew3
+#    define cupsGetDests           cupsGetDests2
+#    define cupsRasterReadHeader   cupsRasterReadHeader2
+#    define cupsRasterWriteHeader  cupsRasterWriteHeader2
+#    define httpConnect            httpConnect2
+#    define ippGetFirstAttribute   ippFirstAttribute
+#    define ippGetNextAttribute    ippNextAttribute
 
+#    define cups_acopy_cb_t       cups_acopy_func_t
+#    define cups_afree_cb_t       cups_afree_func_t
+#    define cups_array_cb_t       cups_array_func_t
+#    define cups_page_header_t    cups_page_header2_t
+#  endif // HAVE_LIBCUPS2
 
-
-#  define cups_acopy_cb_t       cups_acopy_func_t
-#  define cups_afree_cb_t       cups_afree_func_t
-#  define cups_array_cb_t       cups_array_func_t
-#  define cups_page_header_t    cups_page_header2_t
-
-
-# endif // _LIBCUPS2_PRIVATE_H_
+#endif // !_LIBCUPS2_PRIVATE_H_
