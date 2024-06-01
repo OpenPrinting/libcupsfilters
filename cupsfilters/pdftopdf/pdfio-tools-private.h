2dfafc38ca13642c99cc4dfb9c06c4f23968b2cb
ThePhatak <111195860+uddhavphatak@users.noreply.github.com>
2024-06-01 13:47:04 +0530

    Update pdfio-tools-private.h
    Removing commented code for _cfPDFToPDFMakePage

--- cupsfilters/pdftopdf/pdfio-tools-private.h
+++ cupsfilters/pdftopdf/pdfio-tools-private.h
@@ -10,18 +10,5 @@ pdfio_rect_t _cfPDFToPDFGetBleedBox(pdfio_obj_t page);
 pdfio_rect_t _cfPDFToPDFGetTrimBox(pdfio_obj_t page);
 pdfio_rect_t _cfPDFToPDFGetArtBox(pdfio_obj_t page);
 
-/*
-pdfio_obj_t _cfPDFToPDFMakePage(pdfio_file_t &pdf, const std::map<std::string,
-				QPDFObjectHandle &xobjs,
-				QPDFObjectHandle mediabox,
-				const std::string &content);
-*/
-/*
-QPDFObjectHandle _cfPDFToPDFMakePage(QPDF &pdf, const std::map<std::string,
-				     QPDFObjectHandle &xobjs,
-				     QPDFObjectHandle mediabox,
-				     const std::string &content);
-*/
-
 pdfio_rect_t _cfPDFToPDFMakeBox(double x1, double y1, double x2, double y2);
 
