c81ed1302acce2b0ffdaae51497042923bd96a65
uddhavphatak <uddhavabhijeet@gmail.com>
2024-06-01 13:34:25 +0530

    pdfio-tools function defination file(excluding the _cfPDFToPDFMakePage function)

---
+++ cupsfilters/pdftopdf/pdfio-tools.c
@@ -0,0 +1,79 @@
+#include "pdfio-tools-private.h"
+
+pdfio_rect_t
+_cfPDFToPDFGetMediaBox(pdfio_obj_t *page) // {{{
+{
+  pdfio_rect_t mediaBox;
+  pdfio_dict_t *page_dict = pdfioObjGetDict(page);
+  pdfioDictGetRect(page_dict, "MediaBox", &mediaBox);
+  return (mediaBox);
+}
+// }}}
+
+pdfio_rect_t
+_cfPDFToPDFGetCropBox(pdfio_obj_t *page) // {{{
+{
+  pdfio_rect_t cropBox;
+  pdfio_dict_t *page_dict = pdfioObjGetDict(page);
+  if (!pdfioDictGetRect(page_dict, "CropBox", &cropBox)) 
+    return _cfPDFToPDFGetMediaBox(page);
+  return cropBox;
+} 
+// }}}
+
+pdfio_rect_t
+_cfPDFToPDFGetBleedBox(pdfio_obj_t *page) // {{{
+{
+  pdfio_rect_t bleedBox;
+  pdfio_dict_t *page_dict = pdfioObjGetDict(page);
+  if (!pdfioDictGetRect(page_dict, "BleedBox", &bleedBox))
+    return _cfPDFToPDFGetCropBox(page);
+  return bleedBox;
+}
+// }}}
+
+pdfio_rect_t
+_cfPDFToPDFGetTrimBox(pdfio_obj_t *page) // {{{
+{
+  pdfio_rect_t trimBox; 
+  pdfio_dict_t *page_dict = pdfioObjGetDict(page);
+  if (!pdfioDictGetRect(page_dict, "TrimBox", &trimBox)) 
+    return _cfPDFToPDFGetCropBox(page);
+  return trimBox;
+
+}
+// }}}
+
+pdfio_rect_t
+_cfPDFToPDFGetArtBox(pdfio_obj_t *page) // {{{
+{
+  pdfio_rect_t artBox;
+  pdfio_dict_t *page_dict = pdfioObjGetDict(page);
+  if (!pdfioDictGetRect(page_dict, "ArtBox", &artBox)) 
+    return _cfPDFToPDFGetCropBox(page);
+  return artBox;
+}
+// }}}
+
+/*
+pdfio_obj_t 
+_cfPDFToPDFMakePage()
+{
+}
+*/
+
+pdfio_rect_t
+_cfPDFToPDFMakeBox(double x1, 
+		   double y1, 
+		   double x2, 
+		   double y2) // {{{
+{
+  pdfio_rect_t ret;
+  ret.x1 = x1;
+  ret.y1 = y1;
+  ret.x2 = x2;
+  ret.y2 = y2;
+
+  return ret;
+}
+// }}}
