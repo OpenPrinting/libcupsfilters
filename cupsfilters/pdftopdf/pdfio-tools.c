281138854ad746cac3390ef7ff66f0659452e168
uddhavphatak <uddhavabhijeet@gmail.com>
2024-06-04 12:28:25 +0530

    no change

--- cupsfilters/pdftopdf/pdfio-tools.c
+++ cupsfilters/pdftopdf/pdfio-tools.c
@@ -1,7 +1,7 @@
 #include "pdfio-tools-private.h"
 
 pdfio_rect_t
-_cfPDFToPDFGetMediaBox(pdfio_obj_t *page) // {{{
+_cfPDFToPDFGetMediaBox1(pdfio_obj_t *page) // {{{
 {
   pdfio_rect_t mediaBox;
   pdfio_dict_t *page_dict = pdfioObjGetDict(page);
@@ -11,23 +11,23 @@ _cfPDFToPDFGetMediaBox(pdfio_obj_t *page) // {{{
 // }}}
 
 pdfio_rect_t
-_cfPDFToPDFGetCropBox(pdfio_obj_t *page) // {{{
+_cfPDFToPDFGetCropBox1(pdfio_obj_t *page) // {{{
 {
   pdfio_rect_t cropBox;
   pdfio_dict_t *page_dict = pdfioObjGetDict(page);
   if (!pdfioDictGetRect(page_dict, "CropBox", &cropBox)) 
-    return _cfPDFToPDFGetMediaBox(page);
+    return _cfPDFToPDFGetMediaBox1(page);
   return cropBox;
 } 
 // }}}
 
 pdfio_rect_t
-_cfPDFToPDFGetBleedBox(pdfio_obj_t *page) // {{{
+_cfPDFToPDFGetBleedBox1(pdfio_obj_t *page) // {{{
 {
   pdfio_rect_t bleedBox;
   pdfio_dict_t *page_dict = pdfioObjGetDict(page);
   if (!pdfioDictGetRect(page_dict, "BleedBox", &bleedBox))
-    return _cfPDFToPDFGetCropBox(page);
+    return _cfPDFToPDFGetCropBox1(page);
   return bleedBox;
 }
 // }}}
@@ -38,19 +38,19 @@ _cfPDFToPDFGetTrimBox(pdfio_obj_t *page) // {{{
   pdfio_rect_t trimBox; 
   pdfio_dict_t *page_dict = pdfioObjGetDict(page);
   if (!pdfioDictGetRect(page_dict, "TrimBox", &trimBox)) 
-    return _cfPDFToPDFGetCropBox(page);
+    return _cfPDFToPDFGetCropBox1(page);
   return trimBox;
 
 }
 // }}}
 
 pdfio_rect_t
-_cfPDFToPDFGetArtBox(pdfio_obj_t *page) // {{{
+_cfPDFToPDFGetArtBox1(pdfio_obj_t *page) // {{{
 {
   pdfio_rect_t artBox;
   pdfio_dict_t *page_dict = pdfioObjGetDict(page);
   if (!pdfioDictGetRect(page_dict, "ArtBox", &artBox)) 
-    return _cfPDFToPDFGetCropBox(page);
+    return _cfPDFToPDFGetCropBox1(page);
   return artBox;
 }
 // }}}
