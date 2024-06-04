a8fd7dc6e74fea8e1599456990721c138b0277c9
uddhavphatak <uddhavabhijeet@gmail.com>
2024-06-04 12:28:48 +0530

    wrong pointers

--- cupsfilters/pdftopdf/pdfio-tools-private.h
+++ cupsfilters/pdftopdf/pdfio-tools-private.h
@@ -2,13 +2,16 @@
 //
 //
 //
-#include <pdfio.h>
+#ifndef _PDFIO_H_
 
-pdfio_rect_t _cfPDFToPDFGetMediaBox(pdfio_obj_t page);
-pdfio_rect_t _cfPDFToPDFGetCropBox(pdfio_obj_t page);
-pdfio_rect_t _cfPDFToPDFGetBleedBox(pdfio_obj_t page);
-pdfio_rect_t _cfPDFToPDFGetTrimBox(pdfio_obj_t page);
-pdfio_rect_t _cfPDFToPDFGetArtBox(pdfio_obj_t page);
+#define _PDFIO_H_
+#include <pdfio.h>
+#endif
+
+pdfio_rect_t _cfPDFToPDFGetMediaBox1(pdfio_obj_t *page);
+pdfio_rect_t _cfPDFToPDFGetCropBox1(pdfio_obj_t *page);
+pdfio_rect_t _cfPDFToPDFGetBleedBox1(pdfio_obj_t *page);
+pdfio_rect_t _cfPDFToPDFGetTrimBox(pdfio_obj_t *page);
+pdfio_rect_t _cfPDFToPDFGetArtBox1(pdfio_obj_t *page);
 
 pdfio_rect_t _cfPDFToPDFMakeBox(double x1, double y1, double x2, double y2);
-
