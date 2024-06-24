cf11c5e46ddd27b08bfe77f6775723adce1c82c6
uddhavphatak <uddhavabhijeet@gmail.com>
2024-06-24 19:09:30 +0530

    changed variable names

--- cupsfilters/pdftopdf/pdfio-tools-private.h
+++ cupsfilters/pdftopdf/pdfio-tools-private.h
@@ -8,10 +8,10 @@
 #include <pdfio.h>
 #endif
 
-pdfio_rect_t _cfPDFToPDFGetMediaBox1(pdfio_obj_t *page);
-pdfio_rect_t _cfPDFToPDFGetCropBox1(pdfio_obj_t *page);
-pdfio_rect_t _cfPDFToPDFGetBleedBox1(pdfio_obj_t *page);
+pdfio_rect_t _cfPDFToPDFGetMediaBox(pdfio_obj_t *page);
+pdfio_rect_t _cfPDFToPDFGetCropBox(pdfio_obj_t *page);
+pdfio_rect_t _cfPDFToPDFGetBleedBox(pdfio_obj_t *page);
 pdfio_rect_t _cfPDFToPDFGetTrimBox(pdfio_obj_t *page);
-pdfio_rect_t _cfPDFToPDFGetArtBox1(pdfio_obj_t *page);
+pdfio_rect_t _cfPDFToPDFGetArtBox(pdfio_obj_t *page);
 
 pdfio_rect_t _cfPDFToPDFMakeBox(double x1, double y1, double x2, double y2);
