10eb40e34911c1c1495f8fafcd1571583f9cc364
TTFISH <jiongchiyu@gmail.com>
2024-07-12 01:33:40 +0800

    fixed crashed test script
    cupsfilters/test-filter-cases.txt required further checks for the last
    two test cases
    
    Potential output: ERROR: cfFilterUniversal: Unsupported combination of input and output formats: application/pdf -> image/jpeg
    
    Signed-off-by: TTFISH <jiongchiyu@gmail.com>

--- cupsfilters/testfilters.sh
+++ cupsfilters/testfilters.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Test-Suite for Libcupsfilters"
 #Now run the executable file with test case
 echo "Running Tests..."
@@ -5,7 +7,7 @@ echo "Input number of Tests..."
 #read TC
 
 mkdir -p cupsfilters/test_files/output_files
-./testfilters cupsfilters/test-filter-cases.txt 3 2&> Test_summary_final.txt
+./testfilters cupsfilters/test-filter-cases.txt 2 2 2>&1 | tee Test_summary_final.txt
 exit_code=$?
 
 #Delete the output_files folder all together...
