537a848c6f4213bf3feff6d8f0d769d7220a3d5c
uddhavphatak <uddhavabhijeet@gmail.com>
2024-08-12 15:49:53 +0530

    updated function definitions

--- cupsfilters/pdftopdf/pdfio-cm.c
+++ cupsfilters/pdftopdf/pdfio-cm.c
@@ -1,93 +1,105 @@
-#include "pdfio-cm-private.h"  
 #include <stdio.h>
-#include "cupsfilters/debug-internal.h"
+#include <stdlib.h>
 #include <limits.h>
 #include <string.h>
-#include <stdlib.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <pdfio.h>
 #include <pdfio-content.h>
 
 bool 
-_cfPDFToPDFHasOutputIntent(pdfio_file_t *pdf)   // {{{
+_cfPDFToPDFHasOutputIntent(pdfio_file_t *pdf) 
 {
-  pdfio_dict_t *catalogDict = pdfioFileGetCatalog(pdf);
-  if(!pdfioDictGetArray(catalogDict, "OutputIntents"))
+  pdfio_dict_t *catalog = pdfioFileGetCatalog(pdf);
+  if(!pdfioDictGetArray(catalog, "OutputIntents"))
     return false;
-  return true; 
+  return true;
 }
-// }}}
 
 void 
 _cfPDFToPDFAddOutputIntent(pdfio_file_t *pdf, 
-			   const char *filename) //{{{
+			   const char *filename) 
 {
   pdfio_obj_t *outicc = pdfioFileCreateICCObjFromFile(pdf, filename, 4);
-
+  
   pdfio_dict_t *intent = pdfioDictCreate(pdf);
- 
   pdfioDictSetName(intent, "Type", "OutputIntent");
   pdfioDictSetName(intent, "S", "GTS_PDFX");
-  pdfioDictSetString(intent, "OutputCondition", "Commercial and specialty printing");
-  pdfioDictSetString(intent, "Info", "none");
-  pdfioDictSetString(intent, "OutputConditionIdentifier", "CGATS TR001");
-  pdfioDictSetName(intent, "RegistryName", "http://www.color.org");
-  pdfioDictSetNull(intent, "DestOutputProfile");
-  
+  pdfioDictSetString(intent, "OutputCondition", "(Commercial and specialty printing)");
+  pdfioDictSetString(intent, "Info", "(none)");
+  pdfioDictSetString(intent, "OutputConditionIdentifier", "(CGATS TR001i)");
+  pdfioDictSetString(intent, "RegistryName", "(http://www.color.orgi)");
   pdfioDictSetObj(intent, "DestOutputProfile", outicc);
 
-
-  pdfio_dict_t *catalogDict = pdfioFileGetCatalog(pdf);
-  if(!pdfioDictGetArray(catalogDict, "OutputIntents"))
+  pdfio_dict_t *catalog = pdfioFileGetCatalog(pdf);
+  pdfio_array_t *outputIntents = pdfioDictGetArray(catalog, "OutputIntents");
+  if (!outputIntents) 
   {
-    pdfio_array_t *newArray = pdfioArrayCreate(pdf);
-    pdfioDictSetArray(catalogDict, "OutputIntents", newArray);
+    outputIntents = pdfioArrayCreate(pdf);
+    pdfioDictSetArray(catalog, "OutputIntents", outputIntents);
   }
-  pdfioDictSetArray(intent, "OutputIntents", pdfioDictGetArray(catalogDict, "OutputIntents"));
+  
+  pdfioArrayAppendDict(outputIntents, intent);
 }
-//  }}}
 
-void _cfPDFToPDFAddDefaultRGB(pdfio_file_t *pdf, 
-			      pdfio_stream_t *srcicc) //  {{{
+//
+// for color management:
+// Use /DefaultGray, /DefaultRGB, /DefaultCMYK ...  from *current* resource
+// dictionary ...
+// i.e. set 
+// /Resources <<
+// /ColorSpace <<    --- can use just one indirect ref for this (probably)
+// /DefaultRGB [/ICCBased 5 0 R]  ... sensible use is sRGB  for DefaultRGB, etc.
+// >>
+// >>
+// for every page  (what with form /XObjects?)  and most importantly RGB
+// (leave CMYK, Gray for now, as this is already printer native(?))
+//
+// ? also every  form XObject, pattern, type3 font, annotation appearance
+//   stream(=form xobject +X)
+//
+// ? what if page already defines /Default?   -- probably keep!
+//
+// ? maybe we need to set /ColorSpace  in /Images ?
+//   [gs idea is to just add the /Default-key and then reprocess...]
+
+void 
+_cfPDFToPDFAddDefaultRGB(pdfio_file_t *pdf, 
+			 pdfio_obj_t *icc_obj)
 {
+  pdfio_array_t *icc_array = pdfioArrayCreate(pdf);
+    
+  pdfioArrayAppendName(icc_array, "ICCBased");
+  pdfioArrayAppendObj(icc_array, icc_obj);
 
-  int numPages = pdfioFileGetNumPages(pdf);
-  for(int count=0; count<numPages; count++)
+  size_t num_pages = pdfioFileGetNumPages(pdf);
+  for (size_t i = 0; i < num_pages; i++)
   {
-     pdfio_obj_t *tempPage = pdfioFileGetPage(pdf, count+1);
-     pdfio_dict_t *pageDict = pdfioObjGetDict(tempPage);
-    
-     if(!pdfioDictGetDict(pageDict, "Resources"))
-     {
-       pdfio_dict_t *resourcesDict = pdfioDictCreate(pdf);
-       pdfioDictSetDict(pageDict, "Resources", resourcesDict);
-     }
-     pdfio_dict_t *rdict = pdfioDictGetDict(pageDict, "resources");
+    pdfio_obj_t *page_obj = pdfioFileGetPage(pdf, i + 1);  
+    pdfio_dict_t *page_dict = pdfioObjGetDict(page_obj);
 
-     if(!pdfioDictGetDict(rdict, "ColourSpace"))
-     {
-       pdfio_dict_t *colourDict = pdfioDictCreate(pdf);
-       pdfioDictSetDict(rdict, "Resources", colourDict);
-     } 
-     pdfio_dict_t *cdict = pdfioDictGetDict(rdict, "ColourSpace");
-    
-     if(!pdfioDictGetDict(cdict, "DefaultRGB"))
-     {
-       pdfio_array_t *defaultRGB = pdfioArrayCreate(pdf);
-       pdfioArrayAppendName(defaultRGB, "ICCBased");
-       pdfioDictSetArray(cdict, "DefaultRGB", defaultRGB);
-	
-     }
-     break;
+    pdfio_dict_t *rdict = pdfioDictGetDict(page_dict, "Resources");
+    if (!rdict) 
+    {
+      rdict = pdfioDictCreate(pdf);
+      pdfioDictSetDict(page_dict, "Resources", rdict);
+    }
+
+    pdfio_dict_t *cdict = pdfioDictGetDict(rdict, "ColorSpace");
+    if (!cdict) 
+    {
+      cdict = pdfioDictCreate(pdf);
+      pdfioDictSetDict(rdict, "ColorSpace", cdict);
+    }
+
+    pdfioDictSetArray(cdict, "DefaultRGB", icc_array);
   }
 }
-// }}}
 
 
-pdfio_stream_t* 
+pdfio_obj_t*
 _cfPDFToPDFSetDefaultICC(pdfio_file_t *pdf, 
-			 const char *filename)// {{{
+			 const char *filename) 
 {
-	pdfio_obj_t *retobj = pdfioFileCreateICCObjFromFile(pdf, filename, 3);
-	pdfio_stream_t *ret = pdfioObjCreateStream(retobj, PDFIO_FILTER_NONE);
-	return ret;
+    return pdfioFileCreateICCObjFromFile(pdf, filename, 3);
 }
-//  }}}
