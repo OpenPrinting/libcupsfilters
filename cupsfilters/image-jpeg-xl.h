9ee3a7e65cbf97b39202ae13c62634429734d1b6
Titiksha Bansal <129615682+TitikshaBansal@users.noreply.github.com>
2025-02-17 18:57:14 +0530

    Update image-jpeg-xl.h
    Updated file to handle the errors encountered while make.

--- cupsfilters/image-jpeg-xl.h
+++ cupsfilters/image-jpeg-xl.h
@@ -5,8 +5,15 @@
 #include <stdio.h>
 #include "image-private.h"  
 
+int is_jpegxl(const unsigned char *header, size_t len);
+
 int _cfImageReadJPEGXL(cf_image_t *img, FILE *fp,
                        cf_icspace_t primary, cf_icspace_t secondary,
                        int saturation, int hue, const cf_ib_t *lut);
+
+cf_image_t *_cfImageOpenJPEGXL(FILE *fp, cf_icspace_t primary,
+                               cf_icspace_t secondary, int saturation,
+                               int hue, const cf_ib_t *lut);
+
 #endif
 #endif 
