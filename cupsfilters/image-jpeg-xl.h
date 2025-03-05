f7528f45b7cba8aecec075607d24dd21822824ea
Titiksha Bansal <129615682+TitikshaBansal@users.noreply.github.com>
2025-03-05 16:45:46 +0530

    Update image-jpeg-xl.h

--- cupsfilters/image-jpeg-xl.h
+++ cupsfilters/image-jpeg-xl.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include "image-private.h"  
 
-int is_jpegxl(const unsigned char *header, size_t len);
+int _cfIsJPEGXL(const unsigned char *header, size_t len);
 
 int _cfImageReadJPEGXL(cf_image_t *img, FILE *fp,
                        cf_icspace_t primary, cf_icspace_t secondary,
