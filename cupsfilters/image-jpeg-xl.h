8ab75600d0923e4193ffde68dc46e340b9544828
Titiksha Bansal <129615682+TitikshaBansal@users.noreply.github.com>
2025-03-12 21:52:48 +0530

    Update image-jpeg-xl.h

--- cupsfilters/image-jpeg-xl.h
+++ cupsfilters/image-jpeg-xl.h
@@ -10,7 +10,6 @@
 //
 //   _cfIsJPEGXL()            - Check if the file header indicates JPEG‑XL format.
 //   _cfImageReadJPEGXL()     - Read a JPEG‑XL image file using libjxl and fill a cf_image_t structure.
-//   _cfImageOpenJPEGXL()     - Open a JPEG‑XL image file and read it into memory.
 //
 
 
@@ -24,9 +23,5 @@ int _cfIsJPEGXL(const unsigned char *header, size_t len);
 int _cfImageReadJPEGXL(cf_image_t *img, FILE *fp,
                        cf_icspace_t primary, cf_icspace_t secondary,
                        int saturation, int hue, const cf_ib_t *lut);
-
-cf_image_t *_cfImageOpenJPEGXL(FILE *fp, cf_icspace_t primary,
-                               cf_icspace_t secondary, int saturation,
-                               int hue, const cf_ib_t *lut);
 
 #endif // HAVE_LIBJXL
