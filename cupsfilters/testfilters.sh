12cbd5a7484a1f894989ae03150e53717b5ee1cc
Shreyansh Tiwari <shreyansharipur@gmail.com>
2026-02-01 00:05:32 +0530

    tests: fix testfilters line counting and missing header
    Fix testfilters.sh to parse and count the test-filter-cases.txt file automatically and in turn removing the need to hardcode the total number of testcases everytime any change is made to test-filter-cases.txt file.
    
    Also added the missing <sys/stat.h> include in header of testfilters.c file(which is required for S_IRUSR/S_IWUSR
    on some platforms).

--- cupsfilters/testfilters.sh
+++ cupsfilters/testfilters.sh
@@ -7,7 +7,13 @@ echo "Input number of Tests..."
 #read TC
 
 mkdir -p cupsfilters/test_files/output_files
-./testfilters cupsfilters/test-filter-cases.txt 21 2 2>&1 | tee Test_summary_final.txt
+TESTFILE="cupsfilters/test-filter-cases.txt"
+
+# Count all the lines in testcase file. This is will automatically count the number of testcases in turn removing the need for
+# hard coding the number of test cases each and everytime someone makes change to test-filter-cases.txt file
+NUM_LINES=$(wc -l < "$TESTFILE")
+
+./testfilters "$TESTFILE" "$NUM_LINES" 2 2>&1 | tee Test_summary_final.txt
 exit_code=$?
 
 #Delete the output_files folder all together...
